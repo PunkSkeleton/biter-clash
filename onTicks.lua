707fd9da2e34d3df4fd21b4e73fdfbad090e7ab7
PunkSkeleton <103780350+PunkSkeleton@users.noreply.github.com>
2023-04-01 16:52:42 +0200

    Fixed that resources were spawning on the basis on game tick instead of actual game length.

--- onTicks.lua
+++ onTicks.lua
@@ -87,7 +87,7 @@ function every60thTick()
 		end
 	end
 	if global["gameStarted"] then
-		if (game.tick < 108000) then
+		if ((game.tick - global["gameStartedTick"]) < 108000) then
 			chestFillRatio = 1 - ((game.tick - global["gameStartedTick"]) / 108000)
 			fillFreeResourceChests(chestFillRatio)
 		end
